d0d90746f381dcf52373dd461cff565cea939d6e
mattialodi0 <mattia.lodi@gmail.com>
2025-09-11 21:57:58 +0200

    best strategy proof

--- game_engine/players/LodoPlayer.py
+++ game_engine/players/LodoPlayer.py
@@ -1,4 +1,5 @@
 from players.Player import Player
+from numpy import average as avg
 
 
 # to play against random (cannot exploit predictions)
@@ -26,7 +27,8 @@ class LodoPlayer(Player):
 
     def make_prediction_last_round(self, game_state):
         cards_values = [card.value() for card in game_state["cards_visible"]]
-        if max(cards_values) > 20:
+        remaining_cards = [v for v in range(0, 41) if v not in cards_values]
+        if max(cards_values) > avg(remaining_cards):
             self.prediction = 0
         else:
             self.prediction = 1
